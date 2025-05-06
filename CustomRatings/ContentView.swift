be3656327d2039cef70c3f9847ab1ed784d53c96
Fevzi Yaşın <fyasin33@yahoo.com>
2025-05-07 00:02:32 +0300

    Initial commit

---
+++ CustomRatings/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomRatings
+//
+//  Created by Fevzi Yasin on 6.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomRatings/CustomRatingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomRatingsApp.swift
+//  CustomRatings
+//
+//  Created by Fevzi Yasin on 6.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomRatingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
